07b340d2d08bafc0a8df8b33165423a62d1bc1d1
Stenio L. de Souza <steniols@gmail.com>
2021-07-09 11:33:46 -0300

    start deploy configs

--- index.js
+++ index.js
@@ -1,14 +1,22 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-
+const cors = require('cors');
+const path = require('path');
 const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-const cors = require('cors');
+if (process.env.NODE_ENV == 'production') {
+  app.use(express.static(path.join(__dirname, 'client/build')));
+}
+// app.use(express.static(path.join(__dirname, 'client/build')));
+
+console.log(path.join(__dirname, 'client/build'));
 
 app.unsubscribe(express.json());
 app.use(cors());
+
+const PORT = process.env.PORT || 5000;
 
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
@@ -30,6 +38,6 @@ app.use('/auth', require('./routes/jwtAuth'));
 app.use('/dashboard', require('./routes/dashboard'));
 app.use('/tasks', require('./routes/tasks'));
 
-app.listen(5000, () => {
-  console.log('Server is running on port http://localhost:5000');
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
